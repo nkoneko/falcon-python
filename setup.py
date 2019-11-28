eb3900d24fd3629968af327bdd2eeda9984e6500
Nekomura, Koneko <ogawanne@gmail.com>
2019-11-28 20:37:54 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='falcon-python',
+  version='0.1.0',
+  author='Nekomura, Koneko',
+  author_email='ogawanne@gmail.com',
+  packages=['falcon'],
+  install_requres=['aiohttp', 'requests']
+)
